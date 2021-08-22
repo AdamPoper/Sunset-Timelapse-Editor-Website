7196b148575795b46b896d43092350719aceab04
Adam Poper <adampoper@gmail.com>
2021-08-22 16:09:39 -0400

    nothing

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const session = require('express-session');
 
 const app = express();
 
-const PORT = 3001;
+const PORT = 5001;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
